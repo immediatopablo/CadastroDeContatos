50756ad56e62853f0b098efc10ba636c48b4c790
Pablo Immediato <immediatopablo@gmail.com>
2024-11-25 10:20:08 -0300

    Ação para confirmar se deseja realmente excluir a conta!

---
+++ CadastroDeContatos/Models/ViewModels/ConfirmPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroDeContatos.ViewModels
+{
+    public class ConfirmPasswordViewModel
+    {
+        [Required(ErrorMessage = "Por favor, insira sua senha.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
